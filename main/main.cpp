34daa430c5743e1165476610a740bab6ca58e730
wasd003 <2206475938@qq.com>
2023-12-28 23:26:35 +0800

    use third_party_lib

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <unistd.h>
+#include <gperftools/profiler.h>
+using namespace std;
+void func1() {
+	int i = 0;
+	while (i < 1000) {
+		++i;
+	}   
+}
 
-int main() 
-{
-    return 0;
+void func2() {
+	int i = 0;
+	while (i < 200000) {
+		++i;
+	}   
+}
+
+void func3() {
+	for (int i = 0; i < 1000; ++i) {
+        func2();
+	}   
+}
+
+int main(){
+    ProfilerStart("test_capture.prof");
+	while(1) {
+		func3();
+        break;
+	}
+    ProfilerStop();
+	return 0;
 }
